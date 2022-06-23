307297be80ad61687e159b3606cc449dbb4f502c
luizAlexandre-ops <84993102+luizAlexandre-ops@users.noreply.github.com>
2022-06-23 17:47:57 -0500

    TelaDePerfil

--- app/src/main/java/com/example/easyfinance/MainActivity.java
+++ app/src/main/java/com/example/easyfinance/MainActivity.java
@@ -1,14 +1,63 @@
 package com.example.easyfinance;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    de.hdodenhof.circleimageview.CircleImageView image;
+    Button btOpen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        image = findViewById(R.id.image_view);
+        btOpen = findViewById(R.id.bt_open);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{
+                            Manifest.permission.CAMERA
+                    },
+                    100);
+        }
+        btOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,100);
+
+            }
+        });
+
+
+
+
+    }
+
+    @SuppressLint("MissingSuperCall")
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        if(requestCode == 100){
+            Bitmap captureImage = (Bitmap) data.getExtras().get("data");
+            image.setImageBitmap(captureImage);
+
+
+        }
+
     }
 }
