4cbaaa69a79c114545a12d5b0bcac88e025f6464
luizAlexandre-ops <84993102+luizAlexandre-ops@users.noreply.github.com>
2022-07-02 18:54:15 -0500

    Spinner da tela de login e ajuste de cores

--- app/src/main/java/com/example/easyfinance/Tela_Historico.java
+++ app/src/main/java/com/example/easyfinance/Tela_Historico.java
@@ -3,12 +3,26 @@ package com.example.easyfinance;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Tela_Historico extends AppCompatActivity {
+
+    private Spinner filter;
+    String[] opcoes ={"Mês atual", "3 meses", "6 meses", "1 ano"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_historico);
+
+        filter = findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(Tela_Historico.this, android.R.layout.simple_spinner_dropdown_item,opcoes);
+        filter.setAdapter(adapter);
+
+
+
     }
+
+
 }
