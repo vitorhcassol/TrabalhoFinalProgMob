a8a4bc53b7daa01d32f89c7df21b780ad1f659a4
luizAlexandre-ops <84993102+luizAlexandre-ops@users.noreply.github.com>
2022-07-02 17:50:34 -0500

    Alterações na tela de perfil, login e cadastro

--- app/src/main/java/com/example/easyfinance/TelaPerfil.java
+++ app/src/main/java/com/example/easyfinance/TelaPerfil.java
@@ -1,9 +1,17 @@
 package com.example.easyfinance;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,12 +20,35 @@ public class TelaPerfil extends AppCompatActivity {
     private Button bttEdit;
     private Button bttHist;
     private Button bttAdd;
+    private Button bttCamera;
+    private de.hdodenhof.circleimageview.CircleImageView image;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_perfil);
+
+
+        if (ContextCompat.checkSelfPermission(TelaPerfil.this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(TelaPerfil.this,
+                    new String[]{
+                            Manifest.permission.CAMERA
+                    },
+                    100);
+        }
+
+        image = findViewById(R.id.imageUser);
+        bttCamera = findViewById(R.id.ButtonChangeImage);
+        bttCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent open_camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(open_camera,100);
+            }
+        });
 
         bttEdit = findViewById(R.id.buttonEdit);
         bttEdit.setOnClickListener(new View.OnClickListener() {
@@ -42,9 +73,6 @@ public class TelaPerfil extends AppCompatActivity {
                 changeAdd();
             }
         });
-
-
-
     }
 
     private void changeEdit() {
@@ -58,5 +86,12 @@ public class TelaPerfil extends AppCompatActivity {
     private void changeAdd() {
         Intent intent = new Intent(this, Tela_Adicionar.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap photo = (Bitmap) data.getExtras().get("data");
+        image.setImageBitmap(photo);
     }
 }
