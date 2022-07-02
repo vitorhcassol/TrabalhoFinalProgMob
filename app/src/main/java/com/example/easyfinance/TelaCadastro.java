69368a3b67f5d1c9750447cc78599d275a6a5467
luizAlexandre-ops <84993102+luizAlexandre-ops@users.noreply.github.com>
2022-07-01 19:41:36 -0500

    Telas de login, cadastro e perfil

---
+++ app/src/main/java/com/example/easyfinance/TelaCadastro.java
@@ -0,0 +1,13 @@
+package com.example.easyfinance;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_cadastro);
+    }
+}
