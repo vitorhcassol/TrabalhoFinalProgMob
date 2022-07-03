6a5b3726e550be42b79c76308cb6e70169d7a1c8
luizAlexandre-ops <84993102+luizAlexandre-ops@users.noreply.github.com>
2022-07-03 15:39:18 -0500

    tela historioc

--- app/src/main/java/com/example/easyfinance/Tela_Historico.java
+++ app/src/main/java/com/example/easyfinance/Tela_Historico.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 
 public class Tela_Historico extends AppCompatActivity {
 
     private Spinner filter;
+    private ListView viewTransacao;
+    private Usuario u;
+    DBHelper helper = new DBHelper(this);
     String[] opcoes ={"Mês atual", "3 meses", "6 meses", "1 ano"};
 
     @Override
@@ -19,6 +23,10 @@ public class Tela_Historico extends AppCompatActivity {
         filter = findViewById(R.id.spinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(Tela_Historico.this, android.R.layout.simple_spinner_dropdown_item,opcoes);
         filter.setAdapter(adapter);
+
+        viewTransacao = findViewById(R.id.listaItens);
+        ArrayAdapter<Transacao> adapter_spinner = new ArrayAdapter<Transacao>(this, android.R.layout.simple_list_item_1,helper.listaTransacoes(u));
+        viewTransacao.setAdapter(adapter_spinner);
 
 
 
