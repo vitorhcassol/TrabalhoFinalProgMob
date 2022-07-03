03be1005b2d380ddce413e9a70f5576a330b4515
luizAlexandre-ops <84993102+luizAlexandre-ops@users.noreply.github.com>
2022-07-02 22:18:53 -0500

    Adição de tela de exclusão, tela de editar transação e alteração na tela de editar perfil

---
+++ app/src/main/java/com/example/easyfinance/Tela_Exclusao.java
@@ -0,0 +1,14 @@
+package com.example.easyfinance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tela_Exclusao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_exclusao);
+    }
+}
